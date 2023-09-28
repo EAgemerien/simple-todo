db29761dffb0da46346b1eeaa2cbb699068223e7
Emmanuel Agemerien <emmanuelagemerien10@gmail.com>
2023-09-28 17:01:26 +0100

    created my Todo Entity (Blueprint)

---
+++ lib/features/todo/domain/entities/todo.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class Todo extends Equatable {
+  final String id;
+  final DateTime createdAt;
+  final String task;
+  final bool isCompleted;
+  const Todo({
+    required this.id,
+    required this.createdAt,
+    required this.task,
+    required this.isCompleted,
+  });
+
+  @override
+  List<Object?> get props => [id, createdAt, task, isCompleted];
+}
